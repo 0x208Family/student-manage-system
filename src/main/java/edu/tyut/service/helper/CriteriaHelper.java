de10442e109d7387472ae05c3d692782944fd3a9
LH <lh0x208mother@163.com>
2020-06-07 15:15:49 +0800

    2020/6/7

--- src/main/java/edu/tyut/service/helper/CriteriaHelper.java
+++ src/main/java/edu/tyut/service/helper/CriteriaHelper.java
@@ -6,11 +6,9 @@ public interface CriteriaHelper {
 
     String CLEAR_SIGNATURE = "clearCriteria";
 
-    @AOPIgnore
     void clearCriteria();
 
     String CREATE_SIGNATURE = "createCriteria";
 
-    @AOPIgnore
     void createCriteria();
 }
